f401941d01fd73812da1a2673719d26d6f08e421
Gaurav Trapasiya <Gaurav.Trapasiya@ibm.com>
2025-06-07 21:50:31 +0530

    headless true

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     browserName: 'chromium',
-    headless: false,
+    headless: true,
   },
 });
 
